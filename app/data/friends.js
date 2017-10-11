08c0cc35756213bef2425d51d99b704b2c44c24c
Jose Arancibia <josearancibia89@gmail.com>
2017-10-10 22:38:32 -0400

    Uploading code

---
+++ app/data/friends.js
@@ -0,0 +1,165 @@
+// Data structure that will store the friends data
+var friendsArray = [
+	{
+		name: 'Scooter',
+		photo: 'http://cdn3-www.dogtime.com/assets/uploads/gallery/30-impossibly-cute-puppies/impossibly-cute-puppy-15.jpg',
+		scores: [
+			'1',
+			'5',
+			'5',
+			'1',
+			'5',
+			'5',
+			'1',
+			'1',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Archer',
+		photo: 'https://i.pinimg.com/736x/1d/ff/72/1dff7206de7b0b811e93a1af9d60457d--cute-husky-puppies-puppy-husky.jpg',
+		scores: [
+			'2',
+			'4',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'2',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Remy',
+		photo: 'https://i.pinimg.com/736x/46/ea/93/46ea9346ef8df3bf00a97f5a91747f33--kitty-cats-cute-adorable-cats.jpg',
+		scores: [
+			'4',
+			'3',
+			'5',
+			'1',
+			'1',
+			'5',
+			'5',
+			'1',
+			'1',
+			'1'
+		]
+	},
+	{
+		name: 'Chester',
+		photo: 'https://seaotters.com/wp-content/uploads/2012/03/628x353-otter-cu-yawn.jpg',
+		scores: [
+			'5',
+			'1',
+			'2',
+			'1',
+			'3',
+			'4',
+			'1',
+			'3',
+			'3',
+			'4'
+		]
+	},
+	{
+		name: 'Jack',
+		photo: 'https://i.pinimg.com/736x/f7/4d/49/f74d494459ae21f73799e1669cdcb2db--cute-ducklings-baby-ducks.jpg',
+		scores: [
+			'1',
+			'1',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'1'
+		]
+	},
+	{
+		name: 'Asuka',
+		photo: 'http://static.tvtropes.org/pmwiki/pub/images/new_asuka_6322.jpg',
+		scores: [
+			'1',
+			'1',
+			'5',
+			'1',
+			'3',
+			'2',
+			'5',
+			'1',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Yellow Diamond',
+		photo: 'http://i0.kym-cdn.com/entries/icons/original/000/020/284/Face.png',
+		scores: [
+			'5',
+			'4',
+			'4',
+			'2',
+			'4',
+			'4',
+			'3',
+			'2',
+			'3',
+			'3'
+		]
+	},
+	{
+		name: 'Sia',
+		photo: 'https://i.ytimg.com/vi/u4z6RMMIGeI/maxresdefault.jpg',
+		scores: [
+			'5',
+			'2',
+			'3',
+			'1',
+			'5',
+			'1',
+			'3',
+			'1',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Kermit the Frog',
+		photo: 'http://static2.businessinsider.com/image/5970c122c50c298d088b5692/everything-to-know-about-the-unexpected-and-dramatic-firing-of-kermit-the-frog-actor-steve-whitmire-after-27-years.jpg',
+		scores: [
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5'
+		]
+	},
+	{
+		name: 'Steven',
+		photo: 'https://upload.wikimedia.org/wikipedia/en/2/22/Steven_Universe.png',
+		scores: [
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1'
+		]
+	}
+];
+
+module.exports = friendsArray;
